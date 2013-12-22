0b52d242ac10126819cdeacc0f8724266c5787eb
Amanda Garlin <amandagarlin@gmail.com>
2013-12-22 16:23:42 -0500

    Update darwin2p0.js

--- darwin2p0.js
+++ darwin2p0.js
@@ -7,31 +7,6 @@ API.on(API.CURATE_UPDATE, grab);
 API.on(API.CHAT, parseChat);
 API.on(API.VOTE_UPDATE, voteMeh);
 
-//RNG function for accessing array elements (to save code)
-function rng(array) {
-    return Math.floor(Math.random() * array.length);
-}
-
-// STAFF COMMANDS
-
-switch (msg) {
-
-	case "!rockout":
-	if (data.fromID == "50aeb40bd6e4a94f7747c5a4") { API.sendChat('woot!'); API.djJoin(); }
-	else { API.sendChat('I\'ll give a human a chance'); }
-	break;
-
-	case "!stagedive":
-	if (API.getUser(data.fromID).permission > 2) { API.sendChat('cannonball!'); API.djLeave(); }
-	else { API.sendChat('Cat\'s can play too!'); }
-	break;
-
-	case "!grab":
-	if (API.getUser(data.fromID).permission > 2) { API.sendChat('snagged!'); grab(); }
-	else { API.sendChat('You\'re not the boss of me!'); }
-	break;
-} 
-
 //Add new users here to include them in the fact list.
 
 var facts = {
@@ -65,22 +40,40 @@ darwin: ['Charles Darwin\'s voyage was on a boat called the \'Beagle\'.',
 }
 
 
-//Functions
+//FUNCTIONS
 
-function grab(obj) {
-  $(".icon-curate").click();
-  $($(".curate").children(".menu").children().children()[0]).mousedown();
-	var media = API.getMedia();
-	alert(obj.user.username + " added " + media.author + " - " + media.title); 
+//RNG function for accessing array elements (to save code)
+function rng(array) {
+    return Math.floor(Math.random() * array.length);
 }
 
+//Check incoming chat messages and do stuff and things
 function parseChat(data) {
+	// STAFF COMMANDS
+
+	switch (msg) {
+
+	case "!rockout":
+	if (data.fromID == "50aeb40bd6e4a94f7747c5a4") { API.sendChat('woot!'); API.djJoin(); }
+	else { API.sendChat('I\'ll give a human a chance'); }
+	break;
+
+	case "!stagedive":
+	if (API.getUser(data.fromID).permission > 2) { API.sendChat('cannonball!'); API.djLeave(); }
+	else { API.sendChat('Cat\'s can play too!'); }
+	break;
+
+	case "!grab":
+	if (API.getUser(data.fromID).permission > 2) { API.sendChat('snagged!'); grab(); }
+	else { API.sendChat('You\'re not the boss of me!'); }
+	break;
+} 
 
     var msg = data.message; //Just to lessen code
 
     if (msg.match(/^!.*facts$/)) {
 
-        var person = msg.substr(1, msg.length-6); //take the string between the '-' and 'facts'
+        var person = msg.substr(1, msg.length-6); //take the string between the '!' and 'facts'
     
         //If the person exists, grab a fact from the array
         if(facts.hasOwnProperty(person)){
@@ -93,8 +86,16 @@ function parseChat(data) {
     }        
 }
 
-//No Meh Gusta
-/* This is called when somebody in the room (including you) votes. It passes an object with a user object and the vote, -1 for negative, 1 for positive. */
+//Grab Song
+function grab(obj) {
+  $(".icon-curate").click();
+  $($(".curate").children(".menu").children().children()[0]).mousedown();
+	var media = API.getMedia();
+	alert(obj.user.username + " added " + media.author + " - " + media.title); 
+}
+
+/*No Meh Gusta
+This is called when somebody in the room (including you) votes. It passes an object with a user object and the vote, -1 for negative, 1 for positive. */
 
 function voteMeh(obj) {
   var vote = obj.vote == 1 ? "woot" : "meh";
