810d38cdfc1ff76afbe9589fae4a5ce1009e8a98
Amanda Garlin <amandagarlin@gmail.com>
2014-02-16 10:50:13 -0500

    Update darwin2p0.js
    Updated for simplification & sharing purposes. :D

--- darwin2p0.js
+++ darwin2p0.js
@@ -10,33 +10,17 @@ API.on(API.VOTE_UPDATE, voteMeh);
 //Add new users here to include them in the fact list.
 
 var facts = {
+	
 cat: ['I\'m named after Darwin Deez!', 
 	'Meow!',
 	'I really like tuna',
-	'Catnip makes me sneeze. Just say no!',
-	'I\'m a Russian Blue American Shorthair mix!',
-	'I was adopted at a shelter - shelter cats rule!',
-	'I\'m neutered! I don\'t know what that means, but that\'s what they told me when I woke up with a tattoo on my belly. \/whatever',
-	'The Russian Blue is a naturally occurring breed that may have originated in the port of Arkhangelsk, Russia.',
-	'Russian Blues are sometimes called Archangel Blues because it\'s believed that sailors took the cats from the Archangel Isles to England and Northern Europe in the 1860s.',
-	'I am the softest kitty! My fur is known as a double coat, with a downy undercoat that\'s equal in length to the guard hairs, which are an even blue with silver tips. Only Russian Blues and the French Chartreux have this type of coat.',
-	'I\'m really smart! Many Russian Blues have been trained to do tricks. We are a very intelligent breed.', 
-	'I\'m a really great friend! Russian Blues have been known to play fetch, and are sensitive to human emotions.',
-	'The Russian Blue is an intelligent, curious, and tranquil animal. We are known for our friendliness, but are shy with strangers.', 
-	'I usually can be quiet, only meowing occasionally, but I can also be very talkative. I\'m really loud when I want my food!!',
-	'I spend about 90% of my awake time cleaning myself.', 
-	'I am normally reserved around strangers which is why I prefer to meet and talk to new people through a computer.',
-	'Anecdotal evidence suggests that the Russian Blue may be better tolerated by friendbeasts with mild to moderate allergies. There is speculation that we produces less glycoprotein Fel d 1, one source of cat allergies. My thicker coat may also trap more of the allergens closer to my skin.'],
-deez: ['Darwin Deez wrote the anthem of Indie Google+, "Radar Detector." That is his sole redeeming quality.','Darwin Deez is who I\'m named after.', 'Darwin Deez is just as crazy in person as you\'d probably expect.', 'Darwin Deez tries really hard to be a 16 year old hipster. The truth is, he\'s got the douche factor down.', 'Should you go to that Deez show near you? Well, is it free? If not, then no.', 'When invited to karaoke, Darwin Deez declined citing an insecurity about his singing voice. This was 20 minutes before the beginning of his set.'],
+	'I was adopted at a shelter - shelter cats rule!'],
+	
+deez: ['Darwin Deez wrote the anthem of Indie Google+, "Radar Detector." That is his sole redeeming quality.','Darwin Deez is who I\'m named after.', 'Darwin Deez is just as crazy in person as you\'d probably expect.'],
+
 darwin: ['Charles Darwin\'s voyage was on a boat called the \'Beagle\'.',
 	'Charles Darwin was born in Shrewsbury, England, on February 12, 1809.',
-	'Chuck Darwin\'s father, Dr. R.W. Darwin, was as a medical doctor, and his grandfather, Dr. Erasmus Darwin, was a renowned botanist. His father hoped he would follow in his footsteps and become a medical doctor, but the sight of blood made Darwin queasy.',
-	'There\'s that whole theory of evolution thing.',
-	'Chuck Darwin ate as many animals as he documented. He reportedly once gagged on a meal of brown owl, writing that the taste was "indescribable." His favorite? armadillos and agouti.',
-	'Unsurprisingly, Charles Darwin scientifically wrote out a list of pros and cons to matrimony. He concluded that he should marry \(unlike Tesla, who remained abstinent in the name of focusing on science\). The irony is that he married his first cousin.',
-	'Charles Darwin really liked to play backgammon.',
-	'I think there\'s some sort of conspiracy about Charles Darwin being Abraham Lincoln since they have the same birthday. They probably both fought vampires, too.',
-	'The Church of England thought Charles Darwin\'s evolutionary theory was derived from or some sort of version of Tibetan Buddhism. LOL. They have since apologized. Well, after he died.']
+	'There\'s that whole theory of evolution thing.']
 }
 
 
@@ -71,7 +55,7 @@ function parseChat(data) {
 	switch (msg) {
 
 	case "!rockout":
-	if (data.fromID == "50aeb40bd6e4a94f7747c5a4") { API.sendChat('woot!'); API.djJoin(); }
+	if (data.fromID == "yourIDhere") { API.sendChat('woot!'); API.djJoin(); }
 	else { API.sendChat('I\'ll give a human a chance'); }
 	break;
 
@@ -93,15 +77,4 @@ function grab(obj) {
   $($(".curate").children(".menu").children().children()[0]).mousedown();
 	var media = API.getMedia();
 	alert(obj.user.username + " added " + media.author + " - " + media.title); 
-}
-
-/*No Meh Gusta
-This is called when somebody in the room (including you) votes. It passes an object with a user object and the vote, -1 for negative, 1 for positive. */
-
-function voteMeh(obj) {
-  var vote = obj.vote == 1 ? "woot" : "meh";
-	if (vote == -1){
-	API.sendChat(user.username + 'no Mehs in this room please! read room info!');
-  	alert(obj.user.username + " voted " + vote);
-	}
 }
