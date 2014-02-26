8476b4f84454f90e0fa12ec3c00a1f763aea4732
Amanda Garlin <amandagarlin@gmail.com>
2014-02-25 19:42:25 -0500

    Update darwin2p0.js

--- darwin2p0.js
+++ darwin2p0.js
@@ -162,10 +162,9 @@ function parseChat(data) {
         }
     }        
 
-	// STAFF COMMANDS
-
 	switch (msg) {
 
+// STAFF COMMANDS
 	case "!rockout":
 	if (data.fromID == "50aeb40bd6e4a94f7747c5a4") { API.sendChat('woot!'); API.djJoin(); }
 	else { API.sendChat('I\'ll give a human a chance'); }
@@ -180,6 +179,149 @@ function parseChat(data) {
 	if (API.getUser(data.fromID).permission > 2) { API.sendChat('snagged!'); grab(); }
 	else { API.sendChat('You\'re not the boss of me!'); }
 	break;
+	
+// OTHER COMMANDS	
+	case "!pun":
+	function(data) {
+	var keywords = ['http://i.imgur.com/zi3j0tU.gif', 
+			'http://i.imgur.com/cQQkxGt.gif', 
+			'http://i.imgur.com/jfqJteB.gif', 
+			'http://i.minus.com/iICaLVh6wn1dV.gif', 
+			'http://i.imgur.com/Zatc0G1.gif', 
+			'http://i.imgur.com/aN0gkxL.gif', 
+			'http://i.imgur.com/wm8YQEb.gif',
+			'http://i.imgur.com/okZfejS.gif' ];
+	var i = rng(keywords);
+	API.sendChat(''+i+''); }
+	
+	case "alot":
+	function(data) {
+	var keywords = ['http://4.bp.blogspot.com/_D_Z-D2tzi14/S8TRIo4br3I/AAAAAAAACv4/Zh7_GcMlRKo/s400/ALOT.png', 
+			'http://getwrit.squarespace.com/storage/ALOT3.png', 
+			'http://angryxer.files.wordpress.com/2012/07/alot5.png', 
+			'http://1.bp.blogspot.com/_D_Z-D2tzi14/S8TZcKXqR-I/AAAAAAAACwg/F7AqxDrPjhg/s320/ALOT13.png', 
+			'http://2.bp.blogspot.com/_D_Z-D2tzi14/S8TiTtIFjpI/AAAAAAAACxQ/HXLdiZZ0goU/s1600/ALOT14.png', 
+			'http://s3.amazonaws.com/theoatmeal-img/comics/oatmeal_day/alot.png', 
+			'http://1.bp.blogspot.com/_D_Z-D2tzi14/S8TflwXvTgI/AAAAAAAACxI/qgd1wYcTWV8/s320/ALOT12.png'];
+	var i = rng(keywords);
+	API.sendChat(''+i+''); }
+	
+	case "!freakout":
+	function(data) {
+	var keywords = ['http://25.media.tumblr.com/tumblr_mczurxdymF1qzlzhuo1_500.gif', 
+			'http://25.media.tumblr.com/tumblr_lnv7iucmCf1qi1xcjo1_400.gif', 
+			'http://media.giphy.com/media/kHPeHPm3liyNW/200.gif', 
+			'http://i.imgur.com/1sxAD5O.gif', 
+			'http://i.imgur.com/cmHUC.gif', 
+			'http://stream1.gifsoup.com/view1/1401971/freakazoid-o.gif', 
+			'http://i.imgur.com/QLdCn.gif',
+			'http://25.media.tumblr.com/tumblr_m301feBjlr1r5kyvmo3_250.gif',
+			'http://gifs.e-nanigans.com/Need%20Moar%20Coffee.gif'];
+	var i = rng(keywords);
+	API.sendChat(''+i+''); }
+	
+	case "!magic8ball":
+	function(data) {
+	var keywords = ['http://i.imgur.com/qs7GMAi.gif', 
+			'http://i.imgur.com/gyPyMeb.gif', 
+			'http://i.imgur.com/jHQI7rH.gif', 
+			'http://i.imgur.com/8pwCOKc.gif', 
+			'http://i.imgur.com/fZKGA5n.gif', 
+			'http://i.imgur.com/LuNA8tK.gif', 
+			'http://i.imgur.com/xliF1AO.gif', 
+			'http://i.imgur.com/kISiDmK.gif', 
+			'http://i.imgur.com/seh6p.gif', 
+			'http://i.imgur.com/p7CQDef.gif', 
+			'http://i.imgur.com/S5QEVrw.gif', 
+			'http://31.media.tumblr.com/51cf32ea6fb9f87859d70abe055f2911/tumblr_mlu343Kr8I1rbhj4go1_500.gif',
+			'http://i.imgur.com/WHhRGdC.gif',
+			'http://i.imgur.com/EEarzy4.gif',
+			'http://24.media.tumblr.com/01bcd663c56c1cd03729adb14c142c3c/tumblr_mj0yagMLZM1qe3aixo1_500.gif' ];
+	var i = rng(keywords);
+	API.sendChat(''+i+''); }
+	
+	case "!pet":
+	function(data) {
+	var keywords = ['purrr :blue_heart:', 
+			'yea, that\'s the spot!', 
+			'chrrrp!', 
+			'YO! HAND OFF THE BELLY!',
+			':two_hearts:', 
+			'http://25.media.tumblr.com/ae2b28e5cdaec542b0b67f3ca9adb9c6/tumblr_mpgvmdTPv31ror0lro1_400.gif',
+			'http://i.imgur.com/UHmRs.gif',
+			'http://img.pandawhale.com/29495-Grumpy-Cat-Tardcat-STAHP-gif-tx6D.gif',
+			'http://25.media.tumblr.com/1915f3dafacd4427f57530f3aee18d0b/tumblr_mk255d5RLi1r4g1p5o1_400.gif',
+			'http://i.imgur.com/JfaEzmJ.gif',
+			'http://25.media.tumblr.com/9a6f79435b246cdd5fd2e0894a5cb41a/tumblr_mpcirm5Riv1svakfso1_400.gif',,
+			'http://25.media.tumblr.com/c99697ade79f5fdc1f847fee82f681a1/tumblr_mlf8zt33uq1qdlh1io1_400.gif' ];
+	var i = rng(keywords);
+	API.sendChat(''+i+''); }
+	
+	case "!macarena":
+	function(data) {
+	var keywords = [ 'http://25.media.tumblr.com/357140bedeb082cb91fb4cf47962ff62/tumblr_mgl6q79R2V1rl7l4eo1_400.gif', 
+			'http://gifs.e-nanigans.com/TT%20Cat.gif',
+			'http://25.media.tumblr.com/553871c33a0c5273fab93367875098e1/tumblr_ml9m6cJ5yR1qhy6c9o1_500.gif',
+			'http://i.imgur.com/mrRXGNA.gif',
+			'http://storage.coolfeed.co/2012/10/Funny-Cat-Images-24-GIFs-coolfeed.co-24.gif',
+			'http://media0.giphy.com/media/ygsIHxE5xSI4o/original.gif',
+			'http://cdn.memegenerator.net/instances/400x/31453517.jpg',
+			'http://farm3.staticflickr.com/2471/3919527968_33cde7a4d1.jpg',
+			'http://stream1.gifsoup.com/view2/1088961/dancing-cat-o.gif',
+			'http://25.media.tumblr.com/tumblr_lqjoufjvpM1qa9xtyo1_500.gif',
+			'http://i.imgur.com/ooWYNEj.gif',
+			'http://i.imgur.com/TzXNWBw.gif',
+			'http://img1.picturescafe.com/pc/gangnam-style/gangnam-style_007.gif',
+			'http://25.media.tumblr.com/8bc9519efeedba4293256f115fcb9aac/tumblr_mpdywemcDj1rjpc4to1_250.gif',
+			'http://24.media.tumblr.com/5b66a62f395d0fe0f51df826ac40e865/tumblr_mpdywemcDj1rjpc4to4_250.gif',
+			'http://25.media.tumblr.com/f2e915ef1f620cb566ba7acdabe21031/tumblr_mp9yr4NuMr1qdjvlao1_500.gif',
+			'http://cdn0.dailydot.com/uploaded/images/original/2012/7/8/fivedeadlines.gif',
+			'eyyyy, macarena!!',
+			'shimmy shimmy cocopuff!' ];
+	var i = rng(keywords);
+	API.sendChat(''+i+''); }
+	
+	case "!protips":
+	function(data) {
+	var keywords = ['http://i.imgur.com/mYNvii9.gif', 
+		'http://i.imgur.com/w0qTK.gif', 
+		'http://i.imgur.com/TMoOzFx.gif',
+		'http://i.imgur.com/dc6SxxJ.gif',
+		'http://i.imgur.com/mjKG8V0.gif',
+		'use twice as much coffee when brewing for iced coffee. the melting ice will make it perfect.', 
+		'pro tip: prefacing information with \'pro tip\' makes you sound like a tool.', 
+		'Parenting: clearly label your baby\'s rectal thermometer.', 
+		'to keep your manicure in top form, apply a beauty oil not only to your cuticles, but under your nails, too.', 
+		'don\'t sweat the small stuff.',  
+		'wearing a really short bathrobe is an excellent way to get your TV room back from your roommates.', 
+		'only real playas send out a batch of good morning texts to their ladies.', 
+		'making a facebook page and buying an expensive camera makes you a professional photographer.', 
+		'guys, girls are not pokemon. you\'re not supposed to catch them all.', 
+		'bullying doesn\'t stop when you stand up for yourself. it stops when you stand up for someone else.', 
+		'if you find someone who is the same kind of weird as you, whether it be romantic or just a friend, sink your teeth in and hold on.', 
+		'in gmail, instead of hitting \'c\' for compose, hit \'d\'d for compose in new tab.', 
+		'use conditioner as shaving cream.', 
+		'if you yell \'I SAID I WAS SORRY\' at someone, they won\'t sit next to you on the bus or train.', 
+		'know when other people are right.', 
+		'if you must chat while the band is playing, stand in the back or go outside.', 
+		'when making a fake ID, don\'t use the name and photo of Vladimir Putin. bit.ly/151kBb7', 
+		'never do business with a company that has fewer followers on twitter than it has employees.', 
+		'before returning to the US, probably a bad idea to give a bunch of media interviews discussing how you\'re a jihadist.', 
+		'when life makes you eat a salad, season it with Oreo crumbs.', 
+		'when swinging a kettlebell, the bell should never go below your knees.', 
+		'nothing \'writes itself\'.', 
+		'when in doubt, listen to the loudest person in the room. do the exact opposite of everything he\/she says.' ];
+	var i = rng(keywords);
+	API.sendChat(''+i+''); }
+	
+	case "hodor":
+	function(data) {
+	var keywords = ['HODOR!', 'hodor?', 'hodor! hodor!', 'hooodoooooor' ];
+	var i = rng(keywords);
+	API.sendChat(''+i+''); }
+	
+	case "!facepalm":
+	API.sendChat('http://25.media.tumblr.com/efd56a3487ba74f273e5412d8f83563a/tumblr_mp2c38n12c1sok7a0o1_400.gif'); }
 	} 
 }
 
